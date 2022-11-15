927c05cf088382c6cb4f6254bbfd90cd69f7942e
Roman <rombos1978@gmail.com>
2022-11-16 00:07:28 +0300

    *6

---
+++ HW_6/config.py
@@ -0,0 +1,21 @@
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+
+DEFAULT_PORT = 7777
+
+ENCODING = 'utf-8'
+
+MAX_CONNECTIONS = 5
+MAX_SIZE_MSG = 1024
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+STATUS = 'status'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+OK = 'ok'
+
+

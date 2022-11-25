ef2ffc1b9ff4af9f9a02e5156963646ea7e96d7f
Roman <rombos1978@gmail.com>
2022-11-25 15:30:05 +0300

    main_final

---
+++ HW_3/client.py
@@ -0,0 +1,68 @@
+import argparse
+import json
+import socket
+import time
+from config import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, STATUS, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from utils import get_message, send_message
+
+"""Реализовать простое клиент-серверное взаимодействие по протоколу JIM (JSON instant
+messaging):
+a. клиент отправляет запрос серверу;
+Функции клиента:
+● сформировать presence-сообщение;
+● отправить сообщение серверу;
+● получить ответ сервера;
+● разобрать сообщение сервера;
+● параметры командной строки скрипта client.py <addr> [<port>]:
+○ addr — ip-адрес сервера;
+○ port — tcp-порт на сервере, по умолчанию 7777."""
+
+
+def create_presence(account_name='Guest', status=None):
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name,
+            STATUS: status
+        }
+    }
+    return out
+
+
+def process_ans(message):
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Client App')
+    parser.add_argument('-a', '--addr', default=DEFAULT_IP_ADDRESS, type=str,
+                        help='enter IP address')
+    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT,
+                        help='enter port number')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    port = args.port
+    server_address = DEFAULT_IP_ADDRESS
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, port))
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ HW_6/log/client_log_config.py
@@ -0,0 +1,20 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os.path
+
+path = os.path.join(os.path.abspath(__file__), '..', "client.log")
+
+logger = logging.getLogger('client')
+
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(module)s %(message)s ")
+
+F_H = logging.handlers.TimedRotatingFileHandler(path, when='D', backupCount=5, encoding='utf-8')
+F_H.setLevel(logging.DEBUG)
+F_H.setFormatter(formatter)
+
+logger.addHandler(F_H)
+logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    logger.info('Starting client')
+    logger.debug('Отладочная информация')

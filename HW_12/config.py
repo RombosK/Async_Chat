4274e66041826cf8521eec85493b6a9f1a9c1b77
Roman <rombos1978@gmail.com>
2022-12-06 12:59:32 +0300

    12/06.12.22/12:59

---
+++ HW_12/config.py
@@ -0,0 +1,56 @@
+
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+
+DEFAULT_PORT = 7777
+
+ENCODING = 'utf-8'
+
+SERVER_CONFIG = 'server.ini'
+
+MAX_CONNECTIONS = 5
+MAX_SIZE_MSG = 1024
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+STATUS = 'status'
+SENDER = 'from'
+DESTINATION = 'to'
+EXIT = 'exit'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+ALERT = 'alert'
+OK = 'ok'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'message_text'
+GET_CONTACTS = 'get_contacts'
+ADD_CONTACT = 'add_contact'
+USER_ID = 'user_id'
+DEL_CONTACT = 'del_contact'
+
+RESPONSE_OK = {RESPONSE: 200}
+RESPONSE_BAD = {
+    RESPONSE: 400,
+    ERROR: None
+}
+
+
+RESPONSE_202 = {
+    RESPONSE: 202,
+    ALERT: None
+}
+
+CONTACTS = {
+    ACTION: None,
+    TIME: None,
+    ACCOUNT_NAME: None,
+    USER_ID: None
+}
+
+
+
+
+
